82b7807c5a5978172da7dac1560fa67b907d9bfe
Cody Marshall <codymstuntsx@gmail.com>
2023-03-07 20:58:13 -0800

    added 30 prompts

---
+++ src/data/prompts.js
@@ -0,0 +1,118 @@
+export const PROMPTS = [
+    {
+        id: 1,
+        prompt: "Standing 2 foot jump",
+    },
+    {
+        id: 2,
+        prompt: "Standing 1 foot jump",
+    },
+    {
+        id: 3,
+        prompt: "Gather step 2 foot jump",
+    },
+    {
+        id: 4,
+        prompt: "Running 1 foot jump",
+    },
+    {
+        id: 5,
+        prompt: "Standing 2 foot drop jump",
+    },
+    {
+        id: 6,
+        prompt: "Standing 1 foot drop jump",
+    },
+    {
+        id: 7,
+        prompt: "Gather step 2 foot drop jump",
+    },
+    {
+        id: 8,
+        prompt: "Running 1 foot drop jump",
+    },
+    {
+        id: 9,
+        prompt: "Wall tack",
+    },
+    {
+        id: 10,
+        prompt: "Wall tack with 2 steps",
+    },
+    {
+        id: 11,
+        prompt: "Wall tack 180",
+    },
+    {
+        id: 12,
+        prompt: "Wall tack 180 with 2 steps",
+    },
+    {
+        id: 13,
+        prompt: "Kong vault",
+    },
+    {
+        id: 14,
+        prompt: "Kong vault plyo",
+    },
+    {
+        id: 15,
+        prompt: "Kong vault stride",
+    },
+    {
+        id: 16,
+        prompt: "Kong vault stride plyo",
+    },
+    {
+        id: 17,
+        prompt: "Step vault",
+    },
+    {
+        id: 18,
+        prompt: "Step vault plyo",
+    },
+    {
+        id: 19,
+        prompt: "Step vault stride",
+    },
+    {
+        id: 20,
+        prompt: "Step vault stride plyo",
+    },
+    {
+        id: 21,
+        prompt: "Step vault drop precision",
+    },
+    {
+        id: 22,
+        prompt: "Step vault drop plyo",
+    },
+    {
+        id: 23,
+        prompt: "Cat back precision",
+    },
+    {
+        id: 24,
+        prompt: "Cat back plyo",
+    },
+    {
+        id: 25,
+        prompt: "Cat back stride",
+    },
+    {
+        id: 26,
+        prompt: "Lache precision",
+    },
+    {
+        id: 27,
+        prompt: "Lache plyo",
+    },
+    {
+        id: 28,
+        prompt: "Lache stride",
+    },
+    {
+        id: 29,
+        prompt: "Lache stride plyo",
+    },   
+];

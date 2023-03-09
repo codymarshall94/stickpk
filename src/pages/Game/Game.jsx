f365a78a4e84a591837ce7f9e55371e337c619d8
Cody Marshall <codymstuntsx@gmail.com>
2023-03-09 14:30:52 -0800

    game container now animates in

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Turn from "../../components/Turn";
 import "./game.css";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Modal from "../../components/Modal";
 import Winner from "../../components/Winner";
 import PlayerDisplay from "../../components/PlayerDisplay";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+const containerVariant = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.5 } },
+  exit: { opacity: 0, transition: { duration: 0.5 } },
+};
 
 const Game = ({ players, setPlayers, prompts, setPrompts }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [winner, setWinner] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (winner) setModalOpen(true);
@@ -24,59 +31,64 @@ const Game = ({ players, setPlayers, prompts, setPrompts }) => {
     setModalOpen(false);
     setWinner(false);
     setPrompts(false);
+    navigate("/");
   };
 
   return (
-    <div className="game-container">
-      <div>
-        <Turn
-          players={players}
-          setPlayers={setPlayers}
-          setWinner={setWinner}
-          prompts={prompts}
-        />
-      </div>
-      <div className="game-btn-container">
-        <button
-          className="game-btn player-btn primary"
-          onClick={() => setModalOpen(true)}
-        >
-          <img
-            src={require("../../icons/icons8-scorecard-24.png")}
-            alt="score"
-          />
-        </button>
-        <Link
-          to="/"
-          className="game-btn player-btn cancel"
-          onClick={() => resetGame()}
-        >
-          <img
-            src={require("../../icons/icons8-logout-rounded-left-24.png")}
-            alt="exit game"
-          />
-        </Link>
-      </div>
-      <AnimatePresence initial={false} mode="out-in">
-        {modalOpen && (
-          <Modal
-            key="modal"
-            closeModal={() => setModalOpen(false)}
+    <AnimatePresence mode="out-in">
+      <motion.div
+        className="game-container"
+        variants={containerVariant}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+      >
+        <div>
+          <Turn
             players={players}
             setPlayers={setPlayers}
-            winner={winner}
-            reset={resetGame}
-            children={
-              winner ? (
-                <Winner players={players} />
-              ) : (
-                <PlayerDisplay players={players} />
-              )
-            }
+            setWinner={setWinner}
+            prompts={prompts}
           />
-        )}
-      </AnimatePresence>
-    </div>
+        </div>
+        <div className="game-btn-container">
+          <Link to="/" className="game-btn cancel" onClick={() => resetGame()}>
+            <img
+              src={require("../../icons/icons8-logout-rounded-left-24.png")}
+              alt="exit game"
+            />
+          </Link>
+          <button
+            className="game-btn primary"
+            onClick={() => setModalOpen(true)}
+          >
+            <img
+              src={require("../../icons/icons8-scorecard-24.png")}
+              alt="score"
+            />
+          </button>
+        </div>
+        <AnimatePresence initial={false} mode="out-in">
+          {modalOpen && (
+            <Modal
+              key="modal"
+              closeModal={() => setModalOpen(false)}
+              players={players}
+              setPlayers={setPlayers}
+              winner={winner}
+              reset={resetGame}
+              children={
+                winner ? (
+                  <Winner players={players} />
+                ) : (
+                  <PlayerDisplay players={players} />
+                )
+              }
+            />
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 

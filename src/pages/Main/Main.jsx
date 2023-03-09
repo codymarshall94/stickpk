66374db64450a8af9f4c742bdd1d2ce4dbb312a9
Cody Marshall <codymstuntsx@gmail.com>
2023-03-09 14:30:41 -0800

    added motion divs for home screen

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,67 @@
 import { Link } from "react-router-dom";
 import "./main.css";
+import { motion, AnimatePresence } from "framer-motion";
+
+const topDropVariants = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: .5,
+      duration: 2,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    opacity: 0,
+  },
+};
+
+const fadeIn = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1,
+      duration: 1,
+    },
+  },
+  exit: {
+    opacity: 0,
+  },
+};
 
 const Main = () => {
   return (
     <div className="main-container">
-      <h1 className="main-title">Stick</h1>
-      <Link className="main-link" to="/setup">Start Game</Link>
+      <motion.h1
+        variants={topDropVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className="main-title"
+      >
+        Stick
+      </motion.h1>
+      <motion.div
+        variants={fadeIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className="main-subtitle"
+      >
+        <Link className="main-link" to="/setup">
+          Start Game
+        </Link>
+      </motion.div>
     </div>
   );
 };

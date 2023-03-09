3590a403d68eb222d57544b0f8586df61dea30d5
Cody Marshall <codymstuntsx@gmail.com>
2023-03-09 14:30:26 -0800

    variants for player changing and notification

--- src/components/Turn.jsx
+++ src/components/Turn.jsx
@@ -3,8 +3,53 @@ import { generatePrompt } from "../utils/generatePrompt";
 import Letter from "./Letter";
 import "../styles/turn.css";
 import Prompt from "./Prompt";
+import { motion } from "framer-motion";
 
 const gameLetters = ["S", "T", "I", "C", "K"];
+
+const notifyVariant = {
+  hidden: {
+    opacity: 0,
+    y: -50,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const nextPlayerVariant = {
+  hidden: {
+    scale: .9,
+    color: "#fff",
+  },
+  visible: {
+    scale: 1,
+    color: "#fff",
+    transition: {
+      type: "spring",
+      stiffness: 120,
+    },
+  },
+  exit: {
+    scale: 0,
+    color: "#fff",
+    transition: {
+      duration: 0.2,
+    },
+  },
+};
 
 const Turn = ({ players, setPlayers, setWinner, prompts }) => {
   const [challengeSet, setChallengeSet] = useState(false);
@@ -13,12 +58,13 @@ const Turn = ({ players, setPlayers, setWinner, prompts }) => {
   const [challengeAttempts, setChallengeAttemps] = useState(0);
   const [showPrompt, setShowPrompt] = useState(false);
   const [prompt, setPrompt] = useState("");
+  const [showPlayer, setShowPlayer] = useState(true);
+  const [notify, setNotify] = useState(false);
+  const [notifyMessage, setNotifyMessage] = useState("");
 
   useEffect(() => {
-    if(prompts === "true") {
-      console.log("here")
+    if (prompts === "true") {
       const newPrompt = generatePrompt();
-      console.log(newPrompt);
       setPrompt(newPrompt);
       setShowPrompt(true);
     }
@@ -28,7 +74,7 @@ const Turn = ({ players, setPlayers, setWinner, prompts }) => {
     if (prompts === "true" && challengeSetter) {
       setPrompt(generatePrompt());
       setShowPrompt(true);
-    } else if (prompts === "true"  && !challengeSetter) {
+    } else if (prompts === "true" && !challengeSetter) {
       setShowPrompt(false);
       setPrompt("");
     }
@@ -70,6 +116,7 @@ const Turn = ({ players, setPlayers, setWinner, prompts }) => {
   };
 
   const nextPlayer = () => {
+    setShowPlayer(false);
     const index = players.findIndex(
       (player) => player.name === currentPlayer.name
     );
@@ -78,9 +125,14 @@ const Turn = ({ players, setPlayers, setWinner, prompts }) => {
     } else {
       setCurrentPlayer(players[index + 1]);
     }
+    setTimeout(() => {
+      setShowPlayer(true);
+    }, 50);
   };
 
   const giveLetter = () => {
+    setNotify(true);
+    setNotifyMessage(`Letter Given to ${currentPlayer.name}`);
     if (currentPlayer.letters.length === 5) return;
     const currentLetters = currentPlayer.letters;
     switch (currentLetters.length) {
@@ -105,39 +157,62 @@ const Turn = ({ players, setPlayers, setWinner, prompts }) => {
     if (currentLetters.length === 5) {
       handlePlayerOut();
     }
+    setTimeout(() => {
+      setNotify(false);
+    }, 3000);
     setPlayers(players);
   };
 
   return (
     <div className="turn-container">
-      <h1>{currentPlayer.name}'s Turn</h1>
-      <div className="turn-letters-container">
-        {currentPlayer.letters.map((letter) => (
-          <Letter letter={letter} key={letter} size="large" />
-        ))}
-      </div>
-      {showPrompt && <Prompt text={prompt.prompt} />}
-      <h2>{challengeSet ? "Attempt" : "Setting Challenge"}</h2>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
+      <motion.div
+        className="notify"
+        variants={notifyVariant}
+        initial="hidden"
+        animate={notify ? "visible" : "hidden"}
+        exit="exit"
       >
-        <button
-          className="player-btn success"
-          onClick={() => handleSuccess(true)}
+        <p>{notifyMessage}</p>
+      </motion.div>
+      {showPlayer && (
+        <motion.div
+          className="turn"
+          variants={nextPlayerVariant}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
         >
-          <img src={require("../icons/icons8-done-24.png")} alt="yes" />
-        </button>
-        <button
-          className="player-btn cancel"
-          onClick={() => handleSuccess(false)}
-        >
-          <img src={require("../icons/icons8-close-24.png")} alt="no" />
-        </button>
-      </div>
+          <h1>{currentPlayer.name}'s Turn</h1>
+          <div className="turn-letters-container">
+            {currentPlayer.letters.map((letter) => (
+              <Letter letter={letter} key={letter} size="large" />
+            ))}
+          </div>
+          {showPrompt && <Prompt text={prompt.prompt} />}
+          <h2>{challengeSet ? "Attempt" : "Setting Challenge"}</h2>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <button
+              className="turn-btn success"
+              onClick={() => handleSuccess(true)}
+            >
+              <img src={require("../icons/icons8-done-24.png")} alt="yes" />
+            </button>
+            <button
+              className="turn-btn cancel"
+              onClick={() => handleSuccess(false)}
+            >
+              <img src={require("../icons/icons8-close-24.png")} alt="no" />
+            </button>
+          </div>
+        </motion.div>
+      )}
     </div>
   );
 };

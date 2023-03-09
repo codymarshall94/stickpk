50851f996c73e8758d64a4bdbbbb52dd9d4ed7de
Cody Marshall <codymstuntsx@gmail.com>
2023-03-09 15:18:57 -0800

    updated player display to be overflow scroll auto

--- src/components/PlayerDisplay.jsx
+++ src/components/PlayerDisplay.jsx
@@ -3,17 +3,19 @@ import Letter from "./Letter";
 const PlayerDisplay = ({ players }) => {
   return (
     <>
-    <h1>Scoreboard</h1>
-      {players.map((player) => (
-        <div className="player-container" key={player.id}>
-          <p className="player-name">{player.name}</p>
-          <div className="letters-container">
-            {player.letters.map((letter) => (
-              <Letter key={letter} letter={letter} size="small" />
-            ))}
+      <h1>Scoreboard</h1>
+      <div className="game-player-container">
+        {players.map((player) => (
+          <div className="player-container" key={player.id}>
+            <p className="player-name">{player.name}</p>
+            <div className="letters-container">
+              {player.letters.map((letter) => (
+                <Letter key={letter} letter={letter} size="small" />
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   );
 };
